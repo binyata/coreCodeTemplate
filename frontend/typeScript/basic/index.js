eb9b3379aac986bf76e6a11534c1fe5dd1058099
Ryan Lee <ryan.lee@oneclickretail.com>
2018-12-12 12:37:06 -0700

    TypeScript: Basic -- Added Docker file, html, and typescript file for MVP

---
+++ frontend/typeScript/basic/index.js
@@ -0,0 +1,2 @@
+var stringExample = "hello";
+console.log(stringExample + " with a side of variable!");
---
+++ frontend/typeScript/basic/index.ts
@@ -0,0 +1,4 @@
+let stringExample : String = "hello";
+
+
+console.log(`${stringExample} with a side of variable!`);

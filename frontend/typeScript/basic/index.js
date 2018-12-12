d16a00f671962105d0bfad689c49e0bebea0ac2f
Ryan Lee <ryan.lee@oneclickretail.com>
2018-12-12 14:23:55 -0700

    typescript-basic: added the rest of the files needed. Realized that generic typescript cannot import files well.

--- frontend/typeScript/basic/index.js
+++ frontend/typeScript/basic/index.js
@@ -1,2 +1,14 @@
+var obj1 = /** @class */ (function () {
+    function obj1(title, text) {
+        this.title = title;
+        this.text = text;
+    }
+    obj1.prototype.printSomething = function () {
+        console.log(this.title + " is the title");
+    };
+    return obj1;
+}());
+var test = new obj1("title", "name");
+test.printSomething();
 var stringExample = "hello";
 console.log(stringExample + " with a side of variable!");
--- frontend/typeScript/basic/index.ts
+++ frontend/typeScript/basic/index.ts
@@ -1,4 +1,26 @@
+interface Todo {
+    title: string;
+    text: string;
+    printSomething();
+}
+
+class obj1 implements Todo {
+    
+    title: string;
+    text: string;
+
+    constructor(title: string, text: string) {
+        this.title = title;
+        this.text = text;
+    }
+
+    printSomething(): void {
+        console.log(`${this.title} is the title`);
+    }
+}
+
+
+let test = new obj1("title", "name");
+test.printSomething();
 let stringExample : String = "hello";
-
-
 console.log(`${stringExample} with a side of variable!`);
